e0ea4535981ca3e85aab83409a007f0d436e9e78
Andrzej <andrzuk@gmail.com>
2017-12-21 13:01:00 +0100

    FindById of id in server method.

--- server.js
+++ server.js
@@ -86,9 +86,7 @@ app.get('/api/todo/:id', function (req, res) {
     }
     if (db) {
         db.collection('todos', function (err, collection) {
-            collection.find({
-                _id: ObjectId(req.params.id)
-            }).toArray(function (err, result) {
+            collection.findById(req.params.id).toArray(function (err, result) {
                 res.send(result);
             });
         });
